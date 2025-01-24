333a54294a6700e10292dc3cde8ebf84d530ad48
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2025-01-24 14:18:51 +0000

    Finished out homepage and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get("/dice/2/6", { :controller => "dice", :action => "two_six" })
 
   get("/dice/2/10", { :controller => "dice", :action => "two_ten" })
+
+  get("/", { :controller => "dice", :action => "homepage" })
   
 end
